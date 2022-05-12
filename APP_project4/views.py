ca62a977075d7c869e68183459ef882f17817836
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-05-12 09:56:04 +0000

    adding api logic

--- APP_project4/views.py
+++ APP_project4/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    r = requests.get('https://api.mediastack.com/v1/news?
+    access key = 
+    res = r.json()
+    data = res['data']
+    title = []
+    description = []
+    image = []
+    url = []
+    for i in data:
+        title.append(i['title'])
+        description.append(i['description'])
+        image.append(i['image'])
+        url.append(i['url'])
+    news = zip(title, description, image, url)
+    return render(request, 'APP_project4/index.html', {'news':news} )
+
