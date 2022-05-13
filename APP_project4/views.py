2216f2aebbf2144ddb360727086939cd47eaf441
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-05-13 11:02:56 +0000

    added api key and news sources

--- APP_project4/views.py
+++ APP_project4/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     r = requests.get('https://api.mediastack.com/v1/news?
-    access key = 
+    access key =6662ccdb5fde3649fe287f057a5ef1ec
+    & keywords = war,ukriane,russia
+    & languages = en
+    & sources = cnn,bbc
     res = r.json()
     data = res['data']
     title = []
@@ -16,5 +19,5 @@ def index(request):
         image.append(i['image'])
         url.append(i['url'])
     news = zip(title, description, image, url)
-    return render(request, 'APP_project4/index.html', {'news':news} )
+    return render(request, 'APP_project4/index.html', {'news':news})
 
