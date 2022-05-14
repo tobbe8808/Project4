d2f89cce5ab12b83c615ada4b73c1850e93bb79f
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-05-14 09:48:38 +0000

    fixed bug in request api code

--- APP_project4/views.py
+++ APP_project4/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+
+
 def index(request):
-    r = requests.get('https://api.mediastack.com/v1/news?
-    access key =6662ccdb5fde3649fe287f057a5ef1ec
-    & keywords = war,ukriane,russia
-    & languages = en
-    & sources = cnn,bbc
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=9736ce03a738243b550151174c7a437f&countries=gb')  # noqa: E501
     res = r.json()
+    print(res)
     data = res['data']
     title = []
     description = []
@@ -19,5 +18,5 @@ def index(request):
         image.append(i['image'])
         url.append(i['url'])
     news = zip(title, description, image, url)
-    return render(request, 'APP_project4/index.html', {'news':news})
+    return render(request, 'APP_project4/index.html', {'news': news})
 
