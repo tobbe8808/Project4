f9456b0b648394cc9432b1c069fd847236954b78
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-05-14 13:10:11 +0000

    added the admin again for superuser

--- APP_project4/views.py
+++ APP_project4/views.py
@@ -4,7 +4,7 @@ import requests
 
 
 def index(request):
-    r = requests.get('http://api.mediastack.com/v1/news?access_key=9736ce03a738243b550151174c7a437f&countries=gb')  # noqa: E501
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=9736ce03a738243b550151174c7a437f&countries=gb&keywords=war')  # noqa: E501
     res = r.json()
     print(res)
     data = res['data']
