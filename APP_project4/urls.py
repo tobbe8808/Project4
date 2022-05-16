7533a8c83069d07a61d94f6acc17b40fc1f32312
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-05-16 10:27:11 +0000

    added api to news1/news2.html

--- APP_project4/urls.py
+++ APP_project4/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('news1/', views.news1),
+    path('news2/', views.news2),
     path('admin/', admin.site.urls)
 ]
--- APP_project4/views.py
+++ APP_project4/views.py
@@ -4,7 +4,7 @@ import requests
 
 
 def index(request):
-    r = requests.get('http://api.mediastack.com/v1/news?access_key=9736ce03a738243b550151174c7a437f&countries=gb&keywords=war')  # noqa: E501
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=9736ce03a738243b550151174c7a437f&countries=gb&categories=technology')  # noqa: E501
     res = r.json()
     print(res)
     data = res['data']
@@ -19,4 +19,40 @@ def index(request):
         url.append(i['url'])
     news = zip(title, description, image, url)
     return render(request, 'APP_project4/index.html', {'news': news})
+
+
+def news1(request):
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=9736ce03a738243b550151174c7a437f&countries=gb&categories=sports')  # noqa: E501
+    res = r.json()
+    print(res)
+    data = res['data']
+    title = []
+    description = []
+    image = []
+    url = []
+    for i in data:
+        title.append(i['title'])
+        description.append(i['description'])
+        image.append(i['image'])
+        url.append(i['url'])
+    news = zip(title, description, image, url)
+    return render(request, 'APP_project4/news1.html', {'news': news})
+
+
+def news2(request):
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=9736ce03a738243b550151174c7a437f&countries=gb&categories=science')  # noqa: E501
+    res = r.json()
+    print(res)
+    data = res['data']
+    title = []
+    description = []
+    image = []
+    url = []
+    for i in data:
+        title.append(i['title'])
+        description.append(i['description'])
+        image.append(i['image'])
+        url.append(i['url'])
+    news = zip(title, description, image, url)
+    return render(request, 'APP_project4/news2.html', {'news': news})
 
