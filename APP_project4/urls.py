b1ff21ad752fbb4f8b27ee75c33aa6c266e58200
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-05-17 16:29:52 +0000

    linked all html pages and made a navbar

--- APP_project4/urls.py
+++ APP_project4/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('news1/', views.news1),
-    path('news2/', views.news2),
+    path('', views.index, name="index"),
+    path('news1/', views.news1, name="news1"),
+    path('news2/', views.news2, name="news2"),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
 
