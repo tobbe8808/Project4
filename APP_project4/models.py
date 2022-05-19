8ad4043c1ed8542b2925257d62b7c216638c87f9
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-05-19 13:09:18 +0000

    added logic for likebutton

--- APP_project4/models.py
+++ APP_project4/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    body = models.TextField()
+    liked = models.ManyToManyField(User, default=None, blank=True, related_name="thumbsup")
+    updated = models.DateField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.title)
+
+    @property
+    def num_likes(self):
+        return self.liked.all().count()
+
+
+LIKE_CHOICES = (
+    ('Like', 'Like'),
+    ('Unlike', 'Unlike'),
+)
+
+class Like(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    Post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, default='Like', max_length=10)
+
+    def __str__(self):
+        return str(self.post)
+
