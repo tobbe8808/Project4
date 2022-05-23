9da7ac3d5249b195a9f429a6a6bd7aa180f87f98
tobbe8808 <Tobias_sorensen88@hotmail.se>
2022-05-23 15:12:53 +0000

    removed 2 newsapi from two pages and added starter code for blog posts

--- APP_project4/views.py
+++ APP_project4/views.py
@@ -1,47 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post, Like
 import requests
 # Create your views here.
 
+class home(ListView):
+    model = Post
+    template_name = 'index.html'
 
-def index(request):
-    r = requests.get('http://api.mediastack.com/v1/news?access_key=46f5af82baae677bfc2c20387c07fcb2&countries=gb&categories=technology')  # noqa: E501
-    res = r.json()
-    print(res)
-    data = res['data']
-    title = []
-    description = []
-    image = []
-    url = []
-    for i in data:
-        title.append(i['title'])
-        description.append(i['description'])
-        image.append(i['image'])
-        url.append(i['url'])
-    news = zip(title, description, image, url)
-    return render(request, 'APP_project4/index.html', {'news': news})
+class Articledetails(DetailView):
+    model = Post
+    template_name = 'article_details.html'
 
 
 def news1(request):
-    r = requests.get('http://api.mediastack.com/v1/news?access_key=46f5af82baae677bfc2c20387c07fcb2&countries=gb&categories=sports')  # noqa: E501
-    res = r.json()
-    print(res)
-    data = res['data']
-    title = []
-    description = []
-    image = []
-    url = []
-    for i in data:
-        title.append(i['title'])
-        description.append(i['description'])
-        image.append(i['image'])
-        url.append(i['url'])
-    news = zip(title, description, image, url)
-    return render(request, 'APP_project4/news1.html', {'news': news})
+    return render(request, 'news1.html', {})
 
 
 def news2(request):
-    r = requests.get('http://api.mediastack.com/v1/news?access_key=46f5af82baae677bfc2c20387c07fcb2&countries=gb&categories=science')  # noqa: E501
+    r = requests.get('http://api.mediastack.com/v1/news?access_key=46f5af82baae677bfc2c20387c07fcb2&countries=gb&categories=general')  # noqa: E501
     res = r.json()
     print(res)
     data = res['data']
@@ -71,16 +48,16 @@ def post_view(request):
 
 def like_post(request):
     user = request.user
-    if request.metod == 'POST':
+    if request.method == 'POST':
         post_id = request.POST.get('post_id')
         post_obj = Post.objects.get(id=post_id)
 
-        if user in post_obj.linked.all():
+        if user in post_obj.liked.all():
             post_obj.liked.remove(user)
         else:
             post_obj.liked.add(user)
 
-        like, created = Like.objects.get-or_create(user=user, post_id=post_id)
+        like, created = Like.objects.get_or_create(user=user, post_id=post_id)
 
         if not created:
             if like.value == 'Like':
@@ -89,8 +66,5 @@ def like_post(request):
                 like.value = 'Like'
 
         like.save()
-    return redirect('post:post-list')
+    return redirect('posts:post-list')
 
-
-
-    return redirect('post:post-list')
